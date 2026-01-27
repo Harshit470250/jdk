98a00a6ba1d4fcea22f20c733cde8091157f31e8
Harshit Dhiman <harshitdhiman1402@gmail.com>
2026-01-27 12:39:52 +0530

    make _clone_type_Type private

--- src/hotspot/share/opto/runtime.hpp
+++ src/hotspot/share/opto/runtime.hpp
@@ -199,6 +199,7 @@ class OptoRuntime : public AllStatic {
 #endif // INCLUDE_JVMTI
   static const TypeFunc* _dtrace_method_entry_exit_Type;
   static const TypeFunc* _dtrace_object_alloc_Type;
+  static const TypeFunc* _clone_type_Type;
 
   // define stubs
   static address generate_stub(ciEnv* ci_env, TypeFunc_generator gen, address C_function, const char* name, StubId stub_id, int is_fancy_jump, bool pass_tls, bool return_pc);
@@ -232,7 +233,10 @@ class OptoRuntime : public AllStatic {
 public:
   static void monitor_notify_C(oopDesc* obj, JavaThread* current);
   static void monitor_notifyAll_C(oopDesc* obj, JavaThread* current);
-  static const TypeFunc* _clone_type_Type;
+  static const TypeFunc* clone_type(){
+    assert(_clone_type_Type != nullptr, "should be initialized");
+    return _clone_type_Type;
+  }
 
 private:
 
